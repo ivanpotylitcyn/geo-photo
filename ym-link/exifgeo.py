a2a22f2e1b7a6dba12f566e7bd8c00d38abf3a35
Иван Потылицын <ivan.phystech@gmail.com>
2023-02-06 12:28:18 +0300

    Geo coords, date, time and link to Yandex.Maps from exif

---
+++ ym-link/exifgeo.py
@@ -0,0 +1,36 @@
+from exif import Image
+import re
+import sys
+
+img_path = sys.argv[1]
+
+def convert(dms):
+    deg, minutes, seconds, direction = re.split('[°\'"]', dms)
+    return (float(deg) + float(minutes) / 60 + float(seconds) / (60 * 60)) * (-1 if direction in ['W', 'S'] else 1)
+
+with open(img_path, 'rb') as src:
+    img = Image(src)
+
+if img.has_exif:
+    info = f" has the EXIF {img.exif_version}"
+else:
+    info = "does not contain any EXIF information"
+
+print(f"Image {src.name}: {info}")
+
+lat = img.gps_latitude
+lon = img.gps_longitude
+
+geoLat = f"{int(lat[0])}°{int(lat[1])}\'{lat[1] % 1 * 60:.1f}\"{img.gps_latitude_ref}"
+geoLon = f"{int(lon[0])}°{int(lon[1])}\'{lon[1] % 1 * 60:.1f}\"{img.gps_longitude_ref}"
+
+latDd = convert(geoLat)
+lonDd = convert(geoLon)
+
+link = f"https://yandex.ru/maps/?pt={lonDd},{latDd}&z=18&l=map"
+
+print()
+print(geoLat, geoLon)
+print(img.datetime_original.replace(':', '-', 2))
+print()
+print(link)
