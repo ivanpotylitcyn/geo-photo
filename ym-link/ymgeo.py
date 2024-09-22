a55f7631fe54e2585a14ec92b1ded91f11f88a84
Иван Потылицын <potylitcyn@phystech.edu>
2024-09-22 15:57:11 +0300

    Update ymgeo.py
    Убрал минусы из южных и западных координат

--- ym-link/ymgeo.py
+++ ym-link/ymgeo.py
@@ -2,17 +2,19 @@ import sys
 import math
 
 def convert(lat, lon):
+    latRef = 'N' if lat > 0 else 'S'
+    lat = abs(lat)
     latFrac, latInt = math.modf(lat)
     latInt = int(latInt)
     latMin = int(math.floor(latFrac * 60))
     latSec = round(latFrac * 60 * 60 - latMin * 60, 1)
-    latRef = 'N' if lat > 0 else 'S'
 
+    lonRef = 'E' if lon > 0 else 'W'
+    lon = abs(lon)
     lonFrac, lonInt = math.modf(lon)
     lonInt = int(lonInt)
     lonMin = int(math.floor(lonFrac * 60))
     lonSec = round(lonFrac * 60 * 60 - lonMin * 60, 1)
-    lonRef = 'E' if lon > 0 else 'W'
 
     geoString = f"{latInt}°{latMin}\'{latSec}\"{latRef} {lonInt}°{lonMin}\'{lonSec}\"{lonRef}"
 
